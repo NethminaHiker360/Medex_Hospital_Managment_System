984d5fa3034644ccbc1a0737d5551aebfe38120b
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-20 12:23:58 +0530

    use jphonix

--- src/com/developerstack/Medex/controller/LoginFormController.java
+++ src/com/developerstack/Medex/controller/LoginFormController.java
@@ -1,4 +1,20 @@
 package com.developerstack.Medex.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ToggleGroup;
+
 public class LoginFormController {
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+    public ToggleGroup AccoutType;
+    public JFXRadioButton rbtnDoctor;
+
+    public void signInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
