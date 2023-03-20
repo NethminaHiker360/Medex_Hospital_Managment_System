90f5cb618febe8fc74f230d14b8686933a19afb3
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-20 12:50:41 +0530

    add user to db

--- src/com/developerstack/Medex/db/Database.java
+++ src/com/developerstack/Medex/db/Database.java
@@ -1,9 +1,25 @@
 package com.developerstack.Medex.db;
 
 import com.developerstack.Medex.dto.UserDto;
+import com.developerstack.Medex.enums.AccountType;
 
 import java.util.ArrayList;
 
 public class Database {
     public static ArrayList<UserDto> userTable=new ArrayList();
+
+
+
+    static {
+        userTable.add(new UserDto
+                ("Ravindu","Nethmina",
+                        "kar@nethu.com","1234",
+                        AccountType.DOCTOR));
+        userTable.add(new UserDto
+                ("Madara","Nethmini",
+                        "mmn@nethu.com","1234",
+                        AccountType.PATIENT));
+    }
+
+
 }
--- src/com/developerstack/Medex/dto/UserDto.java
+++ src/com/developerstack/Medex/dto/UserDto.java
@@ -3,9 +3,60 @@ package com.developerstack.Medex.dto;
 import com.developerstack.Medex.enums.AccountType;
 
 public class UserDto {
-    String firstName;
-    String lastName;
-    String email;
-    String password;
-    AccountType accountType;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private AccountType accountType;
+
+    public UserDto() {
+    }
+
+    public UserDto(String firstName, String lastName, String email, String password, AccountType accountType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.accountType = accountType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
 }
