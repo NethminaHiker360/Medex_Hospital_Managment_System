5be36dfc05f33ded258c1b675f3c6589fe92b3f5
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-21 16:52:30 +0530

    implement doctor registration form

--- src/com/developerstack/Medex/controller/DoctorRegistrationFormController.java
+++ src/com/developerstack/Medex/controller/DoctorRegistrationFormController.java
@@ -1,10 +1,16 @@
 package com.developerstack.Medex.controller;
 
+import com.developerstack.Medex.db.Database;
+import com.developerstack.Medex.dto.DoctorDto;
+import com.developerstack.Medex.enums.Gender;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.scene.Scene;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 public class DoctorRegistrationFormController {
 
@@ -17,7 +23,17 @@ public class DoctorRegistrationFormController {
     public JFXTextField txtLastName;
     public JFXTextField txtNic;
     public JFXTextField txtContact;
+    public JFXButton doctorRegistrationFormPane;
 
     public void submitDataOnAction(ActionEvent actionEvent) {
+        DoctorDto doctorDto=new DoctorDto(txtFirstName.getText(),
+                txtLastName.getText(),txtNic.getText(),
+                txtContact.getText(),txtEmail.getText(),
+                txtSpecialization.getText(),txtAddress.getText(),
+                rbtnMale.isSelected()? Gender.MALE:Gender.FEMALE);
+
+        Database.doctorTable.add(doctorDto);
+        Stage stage = (Stage) doctorRegistrationFormPane.getScene().getWindow();
+        stage.close();
     }
 }
