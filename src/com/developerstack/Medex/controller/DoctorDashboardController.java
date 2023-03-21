23a3b696383069e5ff5aafe792364928503c6a66
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-21 11:38:00 +0530

    set date and time to DoctorDashboard

--- src/com/developerstack/Medex/controller/DoctorDashboardController.java
+++ src/com/developerstack/Medex/controller/DoctorDashboardController.java
@@ -1,28 +1,58 @@
 package com.developerstack.Medex.controller;
 
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import util.Cookie;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DoctorDashboardController {
     public AnchorPane DoctorDashboardContext;
+    public Label lblDate;
+    public Label lblTime;
 
     public void checkUser() throws IOException {
         if(null==Cookie.selectedUser){
             setUi("LoginForm");
         }
     }
+
+    private void intializeData() {
+//        lblDate.setText(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+        lblDate.setText(String.valueOf(LocalDate.now()));
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            LocalTime now = LocalTime.now();
+            String formattedTime = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            lblTime.setText(formattedTime);
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
     public void initialize() throws IOException {
-        checkUser();
+//        checkUser();
+        intializeData();
     }
     private void setUi(String location) throws IOException {
         Stage stage = (Stage) DoctorDashboardContext.getScene().getWindow();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+    }
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) {
     }
 }
 
