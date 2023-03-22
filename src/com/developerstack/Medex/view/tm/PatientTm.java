bd1e94b2c79852bdced6ccbbc9842ce3b8b98230
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-22 09:40:30 +0530

    create patientDto in tm package

---
+++ src/com/developerstack/Medex/view/tm/PatientTm.java
@@ -0,0 +1,95 @@
+package com.developerstack.Medex.view.tm;
+
+import com.developerstack.Medex.enums.Gender;
+
+import java.util.Date;
+
+public class PatientTm {
+    private String nic;
+    private String firstName;
+    private String lastName;
+    private Date dob;
+    private Gender gender;
+    private String address;
+    private int age;
+
+    public PatientTm(String nic, String firstName, String lastName, Date dob, Gender gender, String address, int age, String email) {
+        this.nic = nic;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.gender = gender;
+        this.address = address;
+        this.age = age;
+        this.email = email;
+    }
+
+    public PatientTm() {
+    }
+
+    private String email;
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
