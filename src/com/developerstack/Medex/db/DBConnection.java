4049de92622027a5a2e49a77241dda2e003c0c4d
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-24 20:09:13 +0530

    apply design pattern - make construstor private to avoid create a new class from outside

--- src/com/developerstack/Medex/db/DBConnection.java
+++ src/com/developerstack/Medex/db/DBConnection.java
@@ -1,3 +1,7 @@
 package com.developerstack.Medex.db;
+
 public class DBConnection {
+    private  DBConnection() throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+    }
 }
