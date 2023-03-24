b8a6aa9d169564a10611bbe49fa06a57ed60142c
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-24 20:43:16 +0530

    implement encrypt password

--- src/util/PasswordConfig.java
+++ src/util/PasswordConfig.java
@@ -1,4 +1,9 @@
 package util;
 
+import org.mindrot.BCrypt;
+
 public class PasswordConfig {
+    public String encrypt(String text){
+        return BCrypt.hashpw(text,BCrypt.gensalt(10));
+    }
 }
