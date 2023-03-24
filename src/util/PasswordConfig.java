884e5d161169bf25433eef235e5b222854c242a9
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-24 20:44:49 +0530

    implement decrypt method

--- src/util/PasswordConfig.java
+++ src/util/PasswordConfig.java
@@ -6,4 +6,7 @@ public class PasswordConfig {
     public String encrypt(String text){
         return BCrypt.hashpw(text,BCrypt.gensalt(10));
     }
+    public boolean decrypt(String rawPassword,String encryptPassword{
+        return BCrypt.checkpw(rawPassword,encryptPassword);
+    }
 }
