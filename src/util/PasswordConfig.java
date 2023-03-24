5f661f09025f1e2266c0f112ec5c0819de3cfe25
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-24 20:46:30 +0530

    comments apply

--- src/util/PasswordConfig.java
+++ src/util/PasswordConfig.java
@@ -5,8 +5,10 @@ import org.mindrot.BCrypt;
 public class PasswordConfig {
     public String encrypt(String text){
         return BCrypt.hashpw(text,BCrypt.gensalt(10));
+        // return encrypt password
     }
     public boolean decrypt(String rawPassword,String encryptPassword{
         return BCrypt.checkpw(rawPassword,encryptPassword);
+        // check password is correct or not
     }
 }
