a2d95287e4dcdc16b65a548f1f0f52c14adcb705
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-24 20:30:46 +0530

    apply design pattern

--- src/com/developerstack/Medex/db/DBConnection.java
+++ src/com/developerstack/Medex/db/DBConnection.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+    public static DBConnection dbConnection;
+    private Connection connection;
+
     private  DBConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/nedex", "root", "1234");
