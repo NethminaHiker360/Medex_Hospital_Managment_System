9c0e769c3cf5e74d5279d73ff2551c4c5ae08add
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-03-25 12:31:24 +0530

    remove unneccesarry codes

--- src/com/developerstack/Medex/controller/SignupFormController.java
+++ src/com/developerstack/Medex/controller/SignupFormController.java
@@ -32,44 +32,31 @@ public class SignupFormController {
 
 
     public void signupOnAction(ActionEvent actionEvent) throws IOException {
-        String email=txtEmail.getText();
         AccountType accountType=rbtnDoctor.isSelected()? AccountType.DOCTOR:AccountType.PATIENT;
-        User new_user=new User(txtFirstName.getText(),txtLastName.getText(),txtEmail.getText(),
+        User new_user=new User(txtFirstName.getText(),txtLastName.getText(),txtEmail.getText().trim().toLowerCase(),
                 txtPassword.getText(),accountType);
-        Optional<User> user = Database.userTable.stream().filter(userDto -> userDto.getEmail().equals(email.toLowerCase())).findFirst();
-        if(user.isPresent()){
-            new Alert(Alert.AlertType.WARNING,"Email Already exists!").show();
-        }else {
-            Database.userTable.add(new_user);
-            new Alert(Alert.AlertType.CONFIRMATION,"User Added !").show();
-            setUi("LoginForm");
-            //======================================
-            try {
-                Class.forName("com.mysql.jdbc.cj.Driver");
-                Connection connection = DriverManager.getConnection
-                        ("jdbc:mysql://localhost:3306/medex_home_git",
-                                "root","1234");
-                String sql="INSERT INTO user VALUES (?,?,?,?,?,?)";
-                PreparedStatement pstm = connection.prepareStatement(sql);
-                pstm.setInt(1,1001);
-                pstm.setString(2,new_user.getFirstName());
-                pstm.setString(3,new_user.getLastName());
-                pstm.setString(4,new_user.getEmail());
-                pstm.setString(5,new_user.getPassword());
-                pstm.setString(6,new_user.getAccountType().name());
+        try {
+            Class.forName("com.mysql.jdbc.cj.Driver");
+            Connection connection = DriverManager.getConnection
+                    ("jdbc:mysql://localhost:3306/medex_home_git",
+                            "root","1234");
+            String sql="INSERT INTO user VALUES (?,?,?,?,?,?)";
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setInt(1,1001);
+            pstm.setString(2,new_user.getFirstName());
+            pstm.setString(3,new_user.getLastName());
+            pstm.setString(4,new_user.getEmail());
+            pstm.setString(5,new_user.getPassword());
+            pstm.setString(6,new_user.getAccountType().name());
 
-                int isSaved = pstm.executeUpdate();
-                if (isSaved>0){
-                    new Alert(Alert.AlertType.CONFIRMATION,"Saved!").show();
-                }else {
-                    new Alert(Alert.AlertType.WARNING,"Try Again").show();
-                }
-
-            } catch (ClassNotFoundException|SQLException e) {
-                e.printStackTrace();
+            int isSaved = pstm.executeUpdate();
+            if (isSaved>0){
+                new Alert(Alert.AlertType.CONFIRMATION,"Saved!").show();
+            }else {
+                new Alert(Alert.AlertType.WARNING,"Try Again").show();
             }
-
-            //=====================================
+        } catch (ClassNotFoundException|SQLException e) {
+            e.printStackTrace();
         }
     }
 
